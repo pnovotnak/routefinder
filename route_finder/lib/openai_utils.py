888245c942d515c37a24e6851b0cec20fb3f497b
Peter Novotnak <peter.novotnak@reddit.com>
2023-04-27 23:44:02 -0700

    in good shape, working well

--- route_finder/lib/openai_utils.py
+++ route_finder/lib/openai_utils.py
@@ -17,9 +17,9 @@ def read_key(key_file: str = ".openai-key") -> None | str:
 openai.api_key = os.getenv("OPENAI_API_KEY") or read_key()
 
 
-def openai_comment_maturity_assessment(content: list[str]):
+def openai_comment_maturity_assessment(description: str, comments: list[str], ticks: list[str]):
     """Send the comments to OpenAI to get an assessment of the danger"""
-    base_prompt = """
+    base_prompt = f"""
 I'm evaluating climbing routes. I'm going to send you a list of comments about a route and I would like you to tell me how dangerous it is based on the protection available throughout the route.
 
 I'd like you to grade on the following scale:
@@ -32,13 +32,20 @@ Things to know about climbing:
 - It's normal and safe to protect a route with trees and other natural features
 
 Grading parameters:
+- Runouts of more than 15 feet are an automatic "PG13" rating
+- Runouts of more than 30 feet are an automatic "R" rating
 - If comment(s) mention good protection, gear, or plentiful bolts throughout, then the route should be assumed to be "G"
-- Comments about the approach, descent, and rappel should be ignored
+- Sentiment about the approach, descent, and rappel should be ignored
+- Sentiment about getting off route should be ignored
+- Consider only potential length of fall when analizing sentiment about difficulty of protection
 - Unless routefinding is said to be extremely difficult, it should be ignored
 - Do not make route safety assements based on the behavior of the commenters or community at large. Eg, ignore comments about simulclimbing
 - Ignore information about the route being dirty or wet. It's not relevant with regard to protection
 - A requirement of specialized gear should be noted in reasoning but can be ignored
 - If there is not enough data to tell, respond with the string "UNKNOWN"
+
+Input:
+- Everything is in cronological order
 
 Output:
 - The single highest (closest to "X") grade identified should be returned
@@ -51,16 +58,33 @@ I want results formatted as JSON list in the following way:
 ["{{your danger rating}}", "{{your reasoning}}"]
 
 Escape single quotes in your response with a backslash ("\") character.
-
-Here are the comments, in cronological order:
-
 """
+    messages = [
+        {"role": "user", "content": base_prompt},
+        {"role": "user", "content": "Route description:\n{description}"},
+    ]
+    if comments:
+        comments_prompt = "\n- ".join(comments)
+        messages.append({
+            "role": "user", 
+            "content":f"Comments about the route:\n{comments_prompt}"
+        })
+    if ticks:
+        ticks_prompt = "\n- ".join(ticks)
+        messages.append({
+            "role": "user", 
+            "content": f"Personal notes about completions and attempts:\n{ticks_prompt}"
+        })
+
+    tokens = sum([len((m['content'].split())) for m in messages])
+    log_message = f"{tokens} / 8192 tokens used"
+    if tokens / 8192 > 0.9:
+        logger.warning(log_message)
+    elif tokens / 8192 > 1:
+        logger.error(log_message)
     response = openai.ChatCompletion.create(
         model="gpt-4",
-        messages=[
-            {"role": "user", "content": base_prompt},
-            {"role": "user", "content": f"\n- ".join(content)},
-        ],
+        messages=messages,
     )
     if not (choices := response.get('choices', [])):
         logger.error("No choices from OpenAI: %s", response)
--- route_finder/route_finder.py
+++ route_finder/route_finder.py
@@ -1,11 +1,12 @@
 import csv
 import requests
-from enum import Enum
 import re
 import logging
 from typing import Type
 import sys
+from functools import reduce
 import bs4
+import html
 from lib.openai_utils import openai_comment_maturity_assessment
 
 from functools import cache
@@ -16,22 +17,9 @@ logger = logging.getLogger(__name__)
 
 BASE_URL: Final[str] = "https://www.mountainproject.com"
 ROUTE_URL_RE: Final[re.Pattern] = re.compile(r"^https://www.mountainproject.com/route/([0-9]+)/(\w+)")
+TICK_CLEANUP_RE: Final[re.Pattern] = re.compile("^\W*·\W*([\d]+ pitches)?\W*(?:Lead|Follow)?(?:[^\.]+)?.")
 
-class DANGER_RATINGS(Enum):
-    UNKNOWN = -1
-    G = 0
-    PG13 = 1
-    R = 2
-    X = 3
-
-
-DANGER_REGEXES = {
-    DANGER_RATINGS.G: re.compile(r"(great|good|well) (gear|protect(ed|ion))+|protects well"),
-    DANGER_RATINGS.PG13: re.compile(r"spicy|scary|run( |-)?out"),
-    DANGER_RATINGS.R: re.compile(r"long run( |-)?out|unprotected"),
-    DANGER_RATINGS.X: re.compile(r"very long run( |-)?out|suicidal"),
-}
-
+"6 pitches. Lead / Redpoint. Led everything"
 
 def configure_logging(root_logger):
     root_logger.setLevel(logging.DEBUG)
@@ -49,35 +37,38 @@ def parse_url(url: str) -> tuple[str, str]:
     return int(route_id), slug
 
 
-def get_description(route_id) -> str:
-    return ""
+MaturityRating = Type[str]
+
+def get_description(route_url: str) -> str:
+    response = requests.get(
+        route_url, 
+        allow_redirects=True
+    )
+    response.raise_for_status()
+    soup = bs4.BeautifulSoup(response.content, features="html.parser")
+    maturity_rating = None
+    if title := soup.find("h2", {"class": "mr-2"}):
+        possible_maturity_rating_element = title.contents[-1]
+        if isinstance(possible_maturity_rating_element, bs4.element.NavigableString):
+            maturity_rating = str(possible_maturity_rating_element.text).strip()
+
+    # There seem to be three "fr-view" divs:
+    # - Description
+    # - Location
+    # - Protection
+    description = soup.find("div", {"class": "fr-view"}).text
+    return description, maturity_rating
 
 
-def get_danger(content: list[str]) -> None | DANGER_RATINGS:
-    """ Determine the danger of the route from the content. Highest level is matched first """
-    for c in content:
-        for danger_rating in reversed(DANGER_RATINGS):
-            search_results = DANGER_REGEXES[danger_rating].search(c)
-            if search_results:
-                return danger_rating
-
-
-def get_comments(route_id: int) -> list[str]:
-    referer = f"{BASE_URL}/route/{route_id}/"
+def get_comments(route_id: int | str) -> list[str]:
     response = requests.get(
         f"{BASE_URL}/comments/forObject/Climb-Lib-Models-Route/{route_id}?sortOrder=oldest&showAll=true", 
         allow_redirects=True,
-        headers={
-            # "referer": referer,
-            "authority": "www.mountainproject.com",
-            "user-agent": "'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'"
-        }
     )
     response.raise_for_status()
     if not response.content:
         return ""
     soup = bs4.BeautifulSoup(response.content, features="html.parser")
-    # TODO find second child if there are 3 children ("more..." link)
     comments: list[str] = []
     for comment_container in soup.select('.comment-body'):
         comment = comment_container.find('span', id=lambda x: x and x.endswith('-full')).text
@@ -85,18 +76,34 @@ def get_comments(route_id: int) -> list[str]:
     return comments
 
 
-def get_ticks(route_id: int) -> list[str]:
-    return []
-
+def get_ticks(route_id: int | str) -> list[str]:
+    response = requests.get(
+        f"{BASE_URL}/api/v2/routes/{route_id}/ticks?per_page=250&page=1",
+        allow_redirects=True,
+    )
+    response.raise_for_status()
+    ticks: list[str] = []
+    for tick in response.json().get('data', {}):
+        if not (tick_text := tick.get('text', '')):
+            continue
+        tick_text = html.unescape(tick_text)
+        tick_text = TICK_CLEANUP_RE.sub("", tick_text)
+        tick_text = tick_text.strip()
+        if not tick_text:
+            continue
+        ticks.append(tick_text)
+    return ticks
 
 Description = Type[str]
 Comments = list[str]
 Ticks = list[str]
 
-def get_beta(route_url) -> tuple[Description, Comments, Ticks]:
+def get_beta(route_url) -> tuple[MaturityRating | None, Description, Comments, Ticks]:
     route_id, _ = parse_url(route_url)
+    description, maturity_rating = get_description(route_url)
     return (
-        get_description(route_id),
+        maturity_rating,
+        description,
         get_comments(route_id),
         get_ticks(route_id),
     )
@@ -112,10 +119,17 @@ def process_csv(csv_in_fn: str = "example.csv", csv_out_fn: str = "example-out.c
         csv_writer.writerow(out_headers)
         for i, row in enumerate(csv_reader):
             row = [route, location, url, avg_stars, your_stars, route_type, yds_difficulty, pitches, length_ft, area_lat, area_lon] = row
-            description, comments, ticks = get_beta(url)
-            maturity_rating, maturity_reason = openai_comment_maturity_assessment(comments) if comments else ["UNKNOWN", "No comments"]
+            logger.info("Processing route \"%s\"", route)
+            maturity_rating, description, comments, ticks = get_beta(url)
+            if not maturity_rating:
+                maturity_rating, maturity_reason = (
+                    openai_comment_maturity_assessment(description, comments, ticks) 
+                    if comments or ticks 
+                    else ["UNKNOWN", "No comments"]
+                )
+            else:
+                maturity_reason = "(Route description)"
             csv_writer.writerow(row[:7] + [maturity_rating, maturity_reason] + row[7:])
-            logger.info("Wrote row for route \"%s\"", route)
 
 
 if __name__ == "__main__":
